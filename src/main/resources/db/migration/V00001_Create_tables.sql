6a24a0c124557f0d1b78c8e5cdaca9a3fe3dc509
Zaza Pipia <zaza.pipia.023@gmail.com>
2025-03-30 12:52:29 +0300

    added table guide

--- src/main/resources/db/migration/V00001_Create_tables.sql
+++ src/main/resources/db/migration/V00001_Create_tables.sql
@@ -23,3 +23,9 @@ CREATE TABLE steam (
     name varchar(50),
     manifest_id varchar(50) UNIQUE NOT NULL
 );
+
+CREATE TABLE guide (
+    id SERIAL PRIMARY KEY,
+    title varchar(100) UNIQUE NOT NULL ,
+    link varchar(100) NOT NULL
+);
